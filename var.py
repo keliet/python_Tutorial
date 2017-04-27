4d72a51beda9abfc651810ff141fcd5660d0c335
keliet <kly-10@qq.com>
2017-04-27 13:59:43 +0800

    Initial commit

---
+++ var.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = '$Package_name'
+__author__ = '$USER'
+__mtime__ = '$DATE'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+"""
